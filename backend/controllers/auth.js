11621adf71161b65d1c335b04c42ef7e5de89e01
iamgodot <xugodot@gmail.com>
2024-03-07 11:02:30 -0800

    Reimplement auth api based on Auth0

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,63 +1,35 @@
 import User from "../models/user.js";
-import bcrypt from "bcryptjs";
-import setCookieToken from "../utils/jwt.js";
-
-export const signup = async (req, res, next) => {
-  try {
-    const { nickname, username, password, passwordConfirmed, gender } =
-      req.body;
-
-    if (password !== passwordConfirmed) {
-      return res
-        .status(400)
-        .json({ error: "Confirmed password does not match" });
-    }
-
-    const userExisted = await User.findOne({ username });
-    if (userExisted) {
-      return res.status(400).json({ error: "User already exists" });
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const passwordHashed = await bcrypt.hash(password, salt);
-    const genderSlug = gender === "male" ? "boy" : "girl";
-    const profilePic = `https://avatar.iran.liara.run/public/${genderSlug}?username=${username}`;
-    const userNew = new User({
-      nickname,
-      username,
-      password: passwordHashed,
-      gender,
-      profilePic,
-    });
-    setCookieToken(userNew._id, res);
-    await userNew.save();
-    res.status(201).json({
-      id: userNew._id,
-      nickname: userNew.nickname,
-      username: userNew.username,
-      profilePic: userNew.profilePic,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
 
 export const login = async (req, res, next) => {
   try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    const isPasswordCorrect = await bcrypt.compare(
-      password,
-      user?.password || "",
-    );
-    if (!user || !isPasswordCorrect) {
-      return res.status(400).json({ error: "Invalid username or password" });
+    const { sub, accessToken } = req.body;
+    if (!sub || !accessToken) {
+      res.status(401).json({ error: "Invalid credentials" });
     }
-    setCookieToken(user._id, res);
+
+    var user = await User.findOne({ username: sub });
+    if (!user) {
+      const resp = await fetch(
+        `https://${process.env.AUTH0_DOMAIN}/api/v2/users/${sub}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      );
+      const { nickname, user_id, email, picture } = await resp.json();
+      user = new User({
+        email,
+        username: user_id,
+        nickname: nickname,
+        profilePic: picture,
+      });
+      await user.save();
+    }
     res.status(200).json({
       id: user._id,
       nickname: user.nickname,
-      username: user.username,
+      email: user.email,
       profilePic: user.profilePic,
     });
   } catch (error) {
@@ -65,6 +37,6 @@ export const login = async (req, res, next) => {
   }
 };
 export const logout = async (req, res) => {
-  res.cookie("jwt", "", { maxAge: 0 });
+  //TODO: record logout
   res.status(200).json({ message: "Logged out" });
 };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,8 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.js";
+import { login, logout } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
