7c807d1b0107b9d2b8ad5e31914f21b3850c3926
iamgodot <xugodot@gmail.com>
2024-03-17 11:14:19 -0700

    Add middleware for protected routes

--- api/middleware.js
+++ api/middleware.js
@@ -1,4 +1,18 @@
+import User from "./models/user.js"
+
 export const errorHandler = (error, req, res, next) => {
     console.log("Error handled:", error.message)
-    res.status(500).json({ error: "Internal server error" })
+    res.status(error.status || 500).json({ error: error.message })
+}
+
+export const authHandler = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ username: req.auth.payload.sub })
+        user.id = user._id
+        req.user = user
+        next()
+    } catch (err) {
+        console.log("Failed to decode user from access token")
+        next(err)
+    }
 }
