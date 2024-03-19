7f26b2117bc8fa302e1178435a8b033e5ebcbff5
iamgodot <xugodot@gmail.com>
2024-03-18 22:04:18 -0700

    User data should have `id` instead of `_id`

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,7 +3,16 @@ import User from "../models/user.js"
 export const getUsers = async (req, res, next) => {
     try {
         const users = await User.find({ _id: { $ne: req.user.id } })
-        res.status(200).json(users)
+        res.status(200).json(
+            users.map((user) => {
+                return {
+                    id: user._id,
+                    nickname: user.nickname,
+                    email: user.email,
+                    profilePic: user.profilePic,
+                }
+            })
+        )
     } catch (error) {
         console.error("Error when getting users", error.message)
         next(error)
