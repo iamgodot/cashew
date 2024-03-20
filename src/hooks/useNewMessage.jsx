82b095371f34da43625e01f514525c18fd81a07c
iamgodot <xugodot@gmail.com>
2024-03-20 14:46:45 -0700

    fix: only update current messages for current conversation

--- src/hooks/useNewMessage.jsx
+++ src/hooks/useNewMessage.jsx
@@ -1,16 +1,20 @@
 import { useSocketContext } from "@/contexts/SocketContext"
 import { useEffect } from "react"
 import useMessages from "./useMessages"
+import { useConversationContext } from "@/contexts/ConversationContext"
 
 const useNewMessage = () => {
     const { socket } = useSocketContext()
     const { currentMessages, setCurrentMessages } = useMessages()
+    const { currentConversation } = useConversationContext()
     useEffect(() => {
         socket?.on("newMessage", (message) => {
-            setCurrentMessages([...currentMessages, message])
+            if (message.senderId === currentConversation.id) {
+                setCurrentMessages([...currentMessages, message])
+            }
         })
         return () => socket?.off("newMessage")
-    }, [socket, currentMessages, setCurrentMessages])
+    }, [socket, currentMessages, setCurrentMessages, currentConversation])
 }
 
 export default useNewMessage
